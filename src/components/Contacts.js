585fa4a5531bc76d87896144a5b1c3e38e60e95f
Ryan <avidryanb@gmail.com>
2019-01-21 07:38:13 -0500

    Checking in progress so far to be cloned on another PC

---
+++ src/components/Contacts.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+class Contacts extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "555-555-5555"
+      },
+      {
+        id: 2,
+        name: "Ryan Boddy",
+        email: "ryanboddyweb@gmail.com",
+        phone: "321-287-7977"
+      },
+      {
+        id: 3,
+        name: "Baylee Boddy",
+        email: "bayleeboddy@gmail.com",
+        phone: "251-223-7515"
+      }
+    ]
+  };
+
+  deleteContact = id => {
+    const { contacts } = this.state;
+
+    const newContacts = contacts.filter(contact => contact.id !== id);
+
+    this.setState({
+      contacts: newContacts
+    });
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <React.Fragment>
+        {contacts.map(contact => (
+          <Contact
+            key={contact.id}
+            contact={contact}
+            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+          />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Contacts;
