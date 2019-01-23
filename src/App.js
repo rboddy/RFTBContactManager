b604fd42cf2bd26d681ad13bd6d80e5d2d0df497
rboddy <avidryanb@gmail.com>
2019-01-23 07:17:49 -0500

    Adding a few more modules worth of work from the course

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Contacts from "./components/Contacts";
-import Header from "./components/Header";
+import Contacts from "./components/contacts/Contacts";
+import Header from "./components/layout/Header";
 import { Provider } from "./context";
+import AddContact from "./components/contacts/AddContact";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -15,6 +16,7 @@ class App extends React.Component {
           <Header branding="Contact Manager" />
 
           <div className="container">
+            <AddContact />
             <Contacts />
           </div>
         </div>
--- src/context.js
+++ src/context.js
@@ -1,38 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 const Context = React.createContext();
 
-export class Provider extends Component {
-    state = {
-        contacts: [
-            {
-              id: 1,
-              name: "John Doe",
-              email: "jdoe@gmail.com",
-              phone: "555-555-5555"
-            },
-            {
-              id: 2,
-              name: "Ryan Boddy",
-              email: "ryanboddyweb@gmail.com",
-              phone: "321-287-7977"
-            },
-            {
-              id: 3,
-              name: "Baylee Boddy",
-              email: "bayleeboddy@gmail.com",
-              phone: "251-223-7515"
-            }
-          ]
-    }
-
-    render(){
-        return (
-            <Context.Provider value={this.state}>
-                {this.props.children}
-            </Context.Provider>
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
         )
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "555-555-5555"
+      },
+      {
+        id: 2,
+        name: "Ryan Boddy",
+        email: "ryanboddyweb@gmail.com",
+        phone: "321-287-7977"
+      },
+      {
+        id: 3,
+        name: "Baylee Boddy",
+        email: "bayleeboddy@gmail.com",
+        phone: "251-223-7515"
+      }
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
     }
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
 }
 
 export const Consumer = Context.Consumer;
