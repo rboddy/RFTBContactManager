61a46f8df893f8981dac682a5b01df22ee73c624
rboddy <avidryanb@gmail.com>
2019-03-04 07:16:12 -0500

    Finished up the context version of the application. Time to move on to redux version so I can learn that. I'll probably make a new repo for it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./components/contacts/Contacts";
 import { Provider } from "./context";
 import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
 import Header from "./components/layout/Header";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
@@ -25,6 +26,7 @@ class App extends React.Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
               </Switch>
